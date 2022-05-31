a19c1002aa619997663ce86701304ff2ed7be4c7
gdufay <gdufay@student.42.fr>
2022-05-31 16:48:02 +0200

    Set bot token as environment variable

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os # get environment variable
+import sys # exit when error
 import argparse # cli handler
 import logging # log for telegram
 
@@ -11,7 +13,6 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Telegram Sprint Bot")
 
     # add arguments to parser
-    parser.add_argument("token", help="Telegram token")
     parser.add_argument("--credential_file", default="credentials.json", help="Path to Google credentials file")
     parser.add_argument("--cookie_file", default="token.json", help="Path to Google token storage file")
     parser.add_argument("--port", help="port from where calendar is listening", type=int)
@@ -20,6 +21,12 @@ def parse_args() -> argparse.Namespace:
 
 def main() -> None:
     """Run the bot."""
+    # get telegram token
+    try:
+        token = os.environ["BOT_TOKEN"]
+    except KeyError:
+        sys.exit("BOT_TOKEN not set")
+
     # get cli arguments
     args = parse_args()
 
@@ -29,7 +36,7 @@ def main() -> None:
     # TODO: catch InvalidToken error
     # TODO: catch other errors
     # launch the bot
-    bot = Bot(args.token, calendar)
+    bot = Bot(token, calendar)
     bot.run_bot()
 
     print("Bye !")
